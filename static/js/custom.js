05f550e501d4afbc39aa1f620f512fe99b754839
Rana Umar Shakoor <120050041+RanaUmarShakoor@users.noreply.github.com>
2023-09-11 03:02:57 +0500

    Jobs carousel added

--- static/js/custom.js
+++ static/js/custom.js
@@ -335,3 +335,26 @@ var btn = $('#myBtn');
       $('html, body').animate({scrollTop:0}, '5000');
     });
 /*** VIDEO JS *****/
+
+
+// Jobs Page Carousel
+var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+var cardWidth = $(".carousel-item").width();
+var scrollPosition = 0;
+
+$(".carousel-control-next").on("click", function () {
+  if (scrollPosition < (carouselWidth - cardWidth * 4)) { //check if you can go any further
+    scrollPosition += cardWidth + 20;  //update scroll position
+    $(".carousel-inner").animate({ scrollLeft: scrollPosition },600); //scroll left
+  }
+});
+
+$(".carousel-control-prev").on("click", function () {
+  if (scrollPosition > 0) {
+    scrollPosition -= cardWidth + 20;
+    $(".carousel-inner").animate(
+      { scrollLeft: scrollPosition },
+      600
+    );
+  }
+});
