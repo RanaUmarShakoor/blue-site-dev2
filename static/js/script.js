377c6a38401b130709ee00f1005b85efe6a85264
Rana Umar Shakoor <120050041+RanaUmarShakoor@users.noreply.github.com>
2023-09-21 20:49:14 +0500

    Icons and 1024px view updated

--- static/js/script.js
+++ static/js/script.js
@@ -1,25 +1,25 @@
-var swiper = new Swiper(".course-slider", {
-    slidesPerView: 2,
-    spaceBetween: 30,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-        320: {
-            slidesPerView: 1,
-            spaceBetween: 0,
-        },
-        576: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
-        768: {
-            slidesPerView: 2,
-            spaceBetween: 30,
-        },
-    },
-});
+// var swiper = new Swiper(".course-slider", {
+//     slidesPerView: 2,
+//     spaceBetween: 30,
+//     navigation: {
+//         nextEl: ".swiper-button-next",
+//         prevEl: ".swiper-button-prev",
+//     },
+//     breakpoints: {
+//         320: {
+//             slidesPerView: 1,
+//             spaceBetween: 0,
+//         },
+//         576: {
+//             slidesPerView: 2,
+//             spaceBetween: 20,
+//         },
+//         768: {
+//             slidesPerView: 2,
+//             spaceBetween: 30,
+//         },
+//     },
+// });
 
 $('button.nav-link').click(function() {
     $('button.nav-link').removeClass('active');
